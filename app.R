a7564482b85ab5eb94e47005df80637c49248b2a
sy25wang <90230188+sy25wang@users.noreply.github.com>
2022-03-11 05:54:03 -0800

    Update app.R

--- app.R
+++ app.R
@@ -9,7 +9,7 @@ app <- Dash$new()
 
 url <- "https://raw.githubusercontent.com/UBC-MDS/vancouver_crime_dashboard/main/data/processed/processed_df.csv"
 crime <- read_csv(url)
-data <- crime |> group_by(TIME, YEAR, Neighborhood) |> count(HOUR) 
+data <- crime  %>% group_by(TIME, YEAR, Neighborhood) %>% count(HOUR) 
 
 feature_mapping <- function(label, value) {
   list(label = label, value = value)
@@ -38,12 +38,12 @@ app$callback(
   output('p', 'figure'),
   list(input('neighborhood-dropdown', 'value')),
   function(neighborhood) {
-    df <- data |> 
+    df <- data  %>% 
       filter(Neighborhood == neighborhood)
     p <- ggplot(df, aes(x=YEAR, n, color=TIME)) + geom_line(stat = 'summary', fun = mean)
     ggplotly(p)
   }
 )
 
-app$run_server(debug = T)
+app$run_server(host = '0.0.0.0')
 
