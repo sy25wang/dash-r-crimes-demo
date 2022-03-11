03d227d03e5bb9e25f9b46ff26af43362024b6f0
sy25wang <90230188+sy25wang@users.noreply.github.com>
2022-03-11 06:02:20 -0800

    Update init.R

--- init.R
+++ init.R
@@ -9,5 +9,5 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'dashHtmlComponents', 'ggplot2', 'plotly', 'dplyr', 'purrr'))
+install.packages(c('dash', 'dashHtmlComponents', 'ggplot2', 'plotly', 'dplyr', 'purrr',  'readr', 'here', 'ggthemes', 'remotes'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
